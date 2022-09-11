f70788e0136ec6252200bb6a9ca1c16b124942c4
qazal <ghazal.nzhrzr@gmail.com>
2022-09-11 14:04:06 +0100

    feat(prisma): seed DB

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,43 +1,70 @@
 import { PrismaClient } from "@prisma/client";
-import { artistsData } from "./songsData";
+import { albums, artistsData, songsData } from "./songsData";
 
 const prisma = new PrismaClient();
-const run = async () => {
-  await Promise.all();
-};
 
 const run = async () => {
+  // Create artists
   await Promise.all(
     artistsData.map(async (artist) => {
       return prisma.artist.upsert({
-        where: { name: artist.name },
+        where: { id: artist.id },
         update: {},
         create: {
           name: artist.name,
-          songs: {
-            create: artist.songs.map((song) => ({
-              name: song.name,
-              duration: song.duration,
-              url: song.url,
-            })),
-          },
+          image: artist.image,
         },
       });
     })
   );
 
-  const salt = bcrypt.genSaltSync();
+  // Create albums
+  await Promise.all(
+    albums.map(async (album) => {
+      return prisma.album.upsert({
+        where: { id: album.id },
+        update: {},
+        create: {
+          name: album.name,
+          image: album.image,
+          artistId: album.artistId,
+          createdAt: album.createdAt,
+        },
+      });
+    })
+  );
+
+  // Create songs
+  await Promise.all(
+    songsData.map(async (s) => {
+      return prisma.song.upsert({
+        where: { id: s.id },
+        update: {},
+        create: {
+          name: s.name,
+          duration: s.duration,
+          url: s.url,
+          albumId: s.albumId,
+          image: s.image,
+          createdAt: s.createdAt,
+        },
+      });
+    })
+  );
+
+  // Create me
   const user = await prisma.user.upsert({
     where: { email: "user@test.com" },
     update: {},
     create: {
-      email: "user@test.com",
-      password: bcrypt.hashSync("password", salt),
-      firstName: "Scott",
-      lastName: "Moss",
+      email: "qazal@dev.com",
+      name: "Qazal",
+      image:
+        "https://i.pinimg.com/564x/4d/34/50/4d34507817d61cccd4d0d4da978972da.jpg",
     },
   });
 
+  // Create playlists
   const songs = await prisma.song.findMany({});
   await Promise.all(
     new Array(10).fill(1).map(async (_, i) => {
--- prisma/songsData.ts
+++ prisma/songsData.ts
@@ -1,9 +1,69 @@
-import { Artist, Song } from "@prisma/client";
+import { Album, Artist, Song } from "@prisma/client";
 
 export const artistsData: Artist[] = [
   {
     id: "1",
-    name: "The Beatles",
-    image: "https://i.scdn.co/image/ab67616d0000b273f2f4d7b2f3f8f4c3b1f2f8f3",
+    name: "Taylor Swift",
+    image:
+      "https://newcountry.nl/images/2022/01/07/taylor-swiftred-taylors-version.jpg",
+  },
+  {
+    id: "2",
+    name: "Harry Styles",
+    image:
+      "https://yt3.ggpht.com/MjEWybBlBXVZigapX__tR_PyJRx-_OGwEZfWZKyS_jJrlgeeF67h69wN2HOhFohiDA7YNeIG=s900-c-k-c0x00ffffff-no-rj",
+  },
+  {
+    id: "3",
+    name: "The Weeknd",
+    image: "https://i.scdn.co/image/ab6761610000f178b5f9e28219c169fd4b9e8379",
+  },
+];
+
+export const albums: Album[] = [
+  {
+    id: "1",
+    name: "reputation",
+    image: "https://media.s-bol.com/MjENqrVV2lvP/vl5k9A5/550x550.jpg",
+    artistId: "1",
+    createdAt: new Date(2018, 10, 10),
+  },
+  {
+    id: "2",
+    name: "Harry's House",
+    image:
+      "https://upload.wikimedia.org/wikipedia/en/d/d5/Harry_Styles_-_Harry%27s_House.png",
+    artistId: "2",
+    createdAt: new Date(2022, 8, 8),
+  },
+  {
+    id: "3",
+    name: "After Hours",
+    image:
+      "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Weeknd_-_After_Hours.png",
+    artistId: "3",
+    createdAt: new Date(2020, 8, 8),
+  },
+];
+
+export const songsData: Song[] = [
+  {
+    id: "2",
+    name: "As It Was",
+    duration: 235,
+    url: "https://dl.dropboxusercontent.com/s/7xmpwvvek6szx5n/fermi-paradox.mp3?dl=0",
+    albumId: "2",
+    image:
+      "https://upload.wikimedia.org/wikipedia/en/d/d5/Harry_Styles_-_Harry%27s_House.png",
+    createdAt: new Date(),
+  },
+  {
+    id: "3",
+    name: "Delicate",
+    duration: 235,
+    url: "https://dl.dropboxusercontent.com/s/7xmpwvvek6szx5n/fermi-paradox.mp3?dl=0",
+    albumId: "1",
+    image: "https://media.s-bol.com/MjENqrVV2lvP/vl5k9A5/550x550.jpg",
+    createdAt: new Date(),
   },
 ];
