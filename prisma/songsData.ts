40e32a9293bb4b9a53f989c817e670e5ddf548c2
qazal <ghazal.nzhrzr@gmail.com>
2022-09-11 13:15:44 +0100

    fix(prisma): minor schema change

---
+++ prisma/songsData.ts
@@ -0,0 +1,9 @@
+import { Artist, Song } from "@prisma/client";
+
+export const artistsData: Artist[] = [
+  {
+    id: "1",
+    name: "The Beatles",
+    image: "https://i.scdn.co/image/ab67616d0000b273f2f4d7b2f3f8f4c3b1f2f8f3",
+  },
+];
