27a8e1b87061d2519bdac416a5bba8c3df321eb9
qazal <ghazal.nzhrzr@gmail.com>
2022-09-14 12:39:54 +0100

    feat(components): reusable menu component

--- src/components/player/AppNavbar.tsx
+++ src/components/player/AppNavbar.tsx
@@ -1,36 +1,21 @@
 import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 import { AiOutlineUser } from "react-icons/ai";
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import { useRouter } from "next/router";
 import { signOut } from "next-auth/react";
+import { MenuDropdown } from "components/shared/MenuDropdown";
 
 export const AppNavbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   // watch for outside clicks and close the menu automagically
-  const wrapperRef = useRef<HTMLDivElement | null>();
-  useEffect(() => {
-    function handleClickOutside(event: MouseEvent) {
-      // what has contains?
-      if (event.target) {
-        // @ts-ignore
-        if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
-          setIsMenuOpen(false);
-        }
-      }
-    }
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [wrapperRef]);
+  const wrapperRef = useRef<HTMLDivElement>(null);
   const router = useRouter();
 
   const userMenuItems = ["profile", "settings"];
   return (
     <div className="w-full h-full relative">
       <PlayerPageControls />
-      {/* @ts-ignore */}
       <div className="flex absolute right-5 mt-2" ref={wrapperRef}>
         <div
           className="bg-stone-700 p-1 rounded-full border-2 border-black visible cursor-pointer"
@@ -38,25 +23,18 @@ export const AppNavbar = () => {
         >
           <AiOutlineUser className="text-xl" />
         </div>
-        <div
-          className={`bg-zinc-800 shadow-lg shadow-black/75 absolute top-9 -left-14 p-3 px-5 rounded-md space-y-2 ${
-            isMenuOpen ? "opacity-1" : "opacity-0"
-          } transition-opacity duration-300`}
+        <MenuDropdown
+          wrapperRef={wrapperRef}
+          options={userMenuItems}
+          onOptionClick={(o) => router.push(`/app/user/${o}`)}
+          isOpen={isMenuOpen}
+          setIsOpen={setIsMenuOpen}
         >
-          {userMenuItems.map((item, i) => (
-            <p
-              key={`usr-menue-${i}`}
-              className="capitalize cursor-pointer"
-              onClick={() => router.push(`/me/${item}`)}
-            >
-              {item}
-            </p>
-          ))}
           <hr />
           <p className="cursor-pointer" onClick={() => signOut()}>
             Log out
           </p>
-        </div>
+        </MenuDropdown>
       </div>
     </div>
   );
---
+++ src/components/shared/MenuDropdown.tsx
@@ -0,0 +1,48 @@
+import {
+  PropsWithChildren,
+  RefObject,
+  Dispatch,
+  SetStateAction,
+  useEffect,
+} from "react";
+
+export const MenuDropdown: React.FC<
+  PropsWithChildren<{
+    wrapperRef: RefObject<HTMLDivElement>;
+    options: string[];
+    onOptionClick: (option: string) => void;
+    isOpen: boolean;
+    setIsOpen: Dispatch<SetStateAction<boolean>>;
+  }>
+> = ({ wrapperRef, options, onOptionClick, children, isOpen, setIsOpen }) => {
+  useEffect(() => {
+    function handleClickOutside(event: any) {
+      if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [wrapperRef, setIsOpen]);
+
+  return (
+    <div
+      className={`bg-zinc-800 shadow-lg shadow-black/75 absolute top-9 -left-14 p-3 px-5 rounded-md space-y-2 ${
+        isOpen ? "opacity-1" : "opacity-0"
+      } transition-opacity duration-300 z-50`}
+    >
+      {options.map((opt, i) => (
+        <p
+          key={`usr-menue-${i}`}
+          className="capitalize cursor-pointer"
+          onClick={() => onOptionClick(opt)}
+        >
+          {opt}
+        </p>
+      ))}
+      {children}
+    </div>
+  );
+};
