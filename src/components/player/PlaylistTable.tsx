79920f7eaa76950cb2c8a4dcb5de290382bcfbed
qazal <ghazal.nzhrzr@gmail.com>
2022-09-18 14:21:46 +0100

    feat(components): playlist table aligned✨

--- src/components/player/PlaylistTable.tsx
+++ src/components/player/PlaylistTable.tsx
@@ -3,16 +3,21 @@ import { AiOutlineClockCircle } from "react-icons/ai";
 export const PlaylistTable = () => {
   return (
     <div className="w-full h-full px-10">
-      <div className="w-full h-full bg-zinc-700 relative">
+      <div className="w-full h-full relative">
         <div className="absolute top-0 w-full flex justify-between h-8 items-center fill-zinc-400 text-zinc-400 uppercase text-sm">
-          <div className="flex">
+          <div className="flex w-1/4">
             <p className="mr-4">#</p>
             <p>title</p>
           </div>
-          <p>album</p>
-          <p>date added</p>
-          <AiOutlineClockCircle className="fill-inherit" />
+          <div className="w-1/4">
+            <p>album</p>
+          </div>
+          <p className="w-1/4">date added</p>
+          <div className="w-1/12 flex justify-end">
+            <AiOutlineClockCircle className="fill-inherit" />
+          </div>
         </div>
+        <hr className="absolute top-8 w-full opacity-20" />
         <div className="top-8 w-full absolute space-y-4 mt-2">
           <Row />
           <Row />
@@ -26,7 +31,7 @@ export const PlaylistTable = () => {
 
 const Row = () => (
   <div className="relative w-full flex justify-between h-10 items-center fill-zinc-400 text-sm text-zinc-400">
-    <div className="flex items-center">
+    <div className="flex items-center w-1/4">
       <p className="mr-4">1</p>
       <div className="flex space-x-2">
         <img
@@ -39,8 +44,8 @@ const Row = () => (
         </div>
       </div>
     </div>
-    <p>Harry's House</p>
-    <p>Jun 10, 2022</p>
-    2:13
+    <p className="w-1/4">Harry's House</p>
+    <p className="w-1/4">Jun 10, 2022</p>
+    <p className="w-1/12 text-end">2:13</p>
   </div>
 );
